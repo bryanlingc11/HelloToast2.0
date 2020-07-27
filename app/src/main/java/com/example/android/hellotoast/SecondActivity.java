d8c11071df6deca93418339c8feedb748bf38762
Bryan Ling <66913969+bryanlingc11@users.noreply.github.com>
2020-07-27 16:56:43 +0800

    committed by Bryan Ling DIB2 G1

---
+++ app/src/main/java/com/example/android/hellotoast/SecondActivity.java
@@ -0,0 +1,21 @@
+package com.example.android.hellotoast;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.textView2);
+        textView.setText(message);
+    }
+}
